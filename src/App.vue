0f39dfb1cf1734072aa1a02947adc75e216b9a4a
bibhu-p <bibhu.p@rrssi.com>
2021-11-19 14:43:28 +0530

    created tasks component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,63 @@
 <template>
   <div class="main_container">
     <Header title="Task Tracker!" />
+    <Tasks
+      @double-click="doubleClicked"
+      @delete-task="deleteTask"
+      v-bind:tasksArr="tasks"
+    />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
+  },
+  methods: {
+    doubleClicked(id) {
+      this.tasks = this.tasks.map(task=>{})
+    },
+    deleteTask(id) {
+      console.log(id);
+    },
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30",
+        reminder: false,
+      },
+      {
+        id: 2,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30",
+        reminder: true,
+      },
+      {
+        id: 4,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30",
+        reminder: true,
+      },
+    ];
   },
 };
 </script>
@@ -19,7 +67,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Tasks.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    @dblclick="doubleClicked(task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task_container']"
+    v-for="task in tasksArr"
+    v-bind:key="task.id"
+  >
+    <div class="task_text">
+      {{ task.text }}
+      <div @click="$emit('delete-task', task.id)">x</div>
+    </div>
+    <div class="task_day">
+      {{ task.day }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tasks",
+  props: {
+    tasksArr: Array,
+  },
+  methods: {
+    doubleClicked(id) {
+      this.$emit("double-click", id);
+    },
+  },
+  emits: ["double-click", "delete-task"],
+};
+</script>
+
+<style scoped>
+.task_container {
+  border: 1px solid gray;
+  padding: 0 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+.task_text {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.task_day {
+  font-size: 0.8rem;
+}
+.reminder {
+  border-left: 3px solid green;
+}
+</style>
